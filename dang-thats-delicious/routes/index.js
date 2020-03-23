59f6be44e562aab7e59cd264299a1ba9c8542119
andrewjohnso7 <andrewjohnso17@gmail.com>
2020-03-23 14:36:26 -0700

    updated endpoints to reach reverse endpoint, also tested out render capability

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -3,7 +3,19 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  const wes = {name: 'Andrew', age: 100, cool:true }
+  // res.json(wes);
+  // res.send('Hey! It works!');
+  // res.send(req.query);
+  res.render('hello', {
+    name: req.query.name,
+    dog: req.query.dog,
+  });
+});
+
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join("");
+  res.send(reverse)
 });
 
 module.exports = router;
