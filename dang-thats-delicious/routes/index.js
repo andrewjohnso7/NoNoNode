344f512187423cc865dab62e2a3d4c454d9a39bf
andrewjohnso7 <andrewjohnso17@gmail.com>
2020-03-24 12:37:28 -0700

    created new routes for adding a store and removed test routes

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController')
 // Do work here
-router.get('/', storeController.myMiddleware, storeController.homePage);
+router.get('/', storeController.homePage);
+router.get('/add', storeController.addStore);
+router.post('/add', storeController.createStore);
 
-router.get('/reverse/:name', storeController.reverse);
 
 module.exports = router;
