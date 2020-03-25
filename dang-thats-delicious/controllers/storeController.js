19a883f97fde94afc1348a848d02bdc7e94020d2
andrewjohnso7 <andrewjohnso17@gmail.com>
2020-03-25 15:19:21 -0700

    created async call for creating a store

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
 exports.homePage = (req, res) => {
   console.log(req.name)
   res.render('index')
@@ -7,7 +10,8 @@ exports.addStore = (req, res) => {
   res.render('editStore', {title: 'Add Store' })
 }
 
-exports.createStore = (req, res) => {
-  // To test this you can console log the request body
-  res.json(req.body);
+exports.createStore = async (req, res) => {
+    const store = await (new Store(req.body)).save();
+    req.flash('success', `Sucessfully Created ${store.name}. Care to leave a review?`);
+    res.redirect(`/store/${store.slug}`)
 }
