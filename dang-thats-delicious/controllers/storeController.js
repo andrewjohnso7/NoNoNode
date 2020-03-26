eecd97533c84432320e53acb5752058fb2fbf98b
andrewjohnso7 <andrewjohnso17@gmail.com>
2020-03-25 17:30:57 -0700

    added logic for editing/updating stores

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -21,3 +21,24 @@ exports.getStores = async (req, res) => {
   const stores = await Store.find();
   res.render('stores', { title: 'Stores', stores })
 }
+
+exports.editStore = async (req, res) => {
+  // 1. find the store given the id
+  const _id = req.params.id
+  const store = await Store.findOne({ _id });
+  // 2. confirm that they are the owner of the store
+  // 3. Render the edit form so that the user can update the store
+  res.render('editStore', {title: `Edit ${store.name}`, store })
+}
+
+exports.updateStore = async (req, res) => {
+  // find and update the store
+  const store = await Store.findOneAndUpdate({ _id: req.params.id, }, req.body, {
+    new:true, // returns the new store instead of the old one
+    runValidators: true
+  }).exec()
+  req.flash('success', `Succesfully updated <strong>${store.name}</strong>.<a href="/stores/${store.slug}> Vie Store</a>`)
+  // redirect them to the store and tell them it worked
+
+  res.redirect(`/stores/${store.id}/edit`)
+}
