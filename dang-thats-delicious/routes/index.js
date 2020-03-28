068578f49b099483bae8aeff7f569079cdf0dfd2
andrewjohnso7 <andrewjohnso17@gmail.com>
2020-03-28 13:14:53 -0700

    added routes with more middleware for fie uploading and resizing

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -7,7 +7,11 @@ router.get('/', catchErrors(storeController.getStores));
 router.get('/stores', catchErrors(storeController.getStores));
 router.get('/add', storeController.addStore);
 router.post('/add', catchErrors(storeController.createStore));
-router.post('/add/:id', catchErrors(storeController.updateStore));
+router.post('/add/:id', 
+  storeController.upload,
+  catchErrors(storeController.resize),
+  catchErrors(storeController.updateStore)
+);
 router.get('/stores/:id/edit', catchErrors(storeController.editStore))
 
 
